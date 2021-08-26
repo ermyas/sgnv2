69bccb4af15ffa40e26d44f1b6cf04131d9b2437
Elliott He <elliott.he@outlook.com>
2021-08-26 17:46:14 +0800

    pass in tmCfg.config when newApp

--- cmd/sgnd.go
+++ cmd/sgnd.go
@@ -2,16 +2,21 @@ package cmd
 
 import (
 	"io"
+	"os"
 	"path/filepath"
 
 	"github.com/celer-network/sgn-v2/app"
 	"github.com/celer-network/sgn-v2/common"
 	"github.com/cosmos/cosmos-sdk/baseapp"
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/config"
 	"github.com/cosmos/cosmos-sdk/server"
+	serverconfig "github.com/cosmos/cosmos-sdk/server/config"
 	servertypes "github.com/cosmos/cosmos-sdk/server/types"
 	simappcmd "github.com/cosmos/cosmos-sdk/simapp/simd/cmd"
 	"github.com/cosmos/cosmos-sdk/store"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/auth/types"
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	"github.com/cosmos/cosmos-sdk/x/crisis"
 	genutilcli "github.com/cosmos/cosmos-sdk/x/genutil/client/cli"
@@ -27,6 +32,36 @@ func GetSgndExecutor() cli.Executor {
 	rootCmd := &cobra.Command{
 		Use:   "sgnd",
 		Short: "SGN App Daemon (server)",
+		PersistentPreRunE: func(cmd *cobra.Command, _ []string) error {
+			// set the default command outputs
+			cmd.SetOut(cmd.OutOrStdout())
+			cmd.SetErr(cmd.ErrOrStderr())
+
+			initClientCtx := client.Context{}.
+				WithCodec(app.AppCodec).
+				WithInterfaceRegistry(app.InterfaceRegistry).
+				WithTxConfig(app.TxConfig).
+				WithLegacyAmino(app.LegacyAmino).
+				WithInput(os.Stdin).
+				WithAccountRetriever(types.AccountRetriever{}).
+				WithHomeDir(app.DefaultNodeHome).
+				WithViper("")
+
+			initClientCtx = client.ReadHomeFlag(initClientCtx, cmd)
+
+			initClientCtx, err := config.ReadFromClientConfig(initClientCtx)
+			if err != nil {
+				return err
+			}
+
+			if err := client.SetCmdClientContextHandler(initClientCtx, cmd); err != nil {
+				return err
+			}
+
+			customAppTemplate, customAppConfig := initAppConfig()
+
+			return server.InterceptConfigsPreRunHandler(cmd, customAppTemplate, customAppConfig)
+		},
 	}
 	// CLI commands to initialize the chain
 	rootCmd.AddCommand(
@@ -37,7 +72,10 @@ func GetSgndExecutor() cli.Executor {
 		simappcmd.AddGenesisAccountCmd(app.DefaultNodeHome),
 	)
 
-	server.AddCommands(rootCmd, app.DefaultNodeHome, newApp, exportAppStateAndTMValidators, addModuleInitFlags)
+	serverCtx := server.GetServerContextFromCmd(rootCmd)
+	a := appCreator{serverCtx}
+
+	server.AddCommands(rootCmd, app.DefaultNodeHome, a.newApp, a.exportAppStateAndTMValidators, addModuleInitFlags)
 	rootCmd.PersistentFlags().String(common.FlagCLIHome, app.DefaultCLIHome, "Directory for cli config and data")
 	rootCmd.PersistentFlags().String(
 		common.FlagConfig, filepath.Join(app.DefaultCLIHome, "config", "sgn.toml"), "Path to SGN-specific configs")
@@ -46,7 +84,11 @@ func GetSgndExecutor() cli.Executor {
 	return cli.PrepareBaseCmd(rootCmd, "SGN", app.DefaultNodeHome)
 }
 
-func newApp(logger tlog.Logger, db dbm.DB, traceStore io.Writer, appOpts servertypes.AppOptions) servertypes.Application {
+type appCreator struct {
+	*server.Context
+}
+
+func (a appCreator) newApp(logger tlog.Logger, db dbm.DB, traceStore io.Writer, appOpts servertypes.AppOptions) servertypes.Application {
 	var cache sdk.MultiStorePersistentCache
 	if cast.ToBool(appOpts.Get(server.FlagInterBlockCache)) {
 		cache = store.NewCommitKVStoreCacheManager()
@@ -65,7 +107,7 @@ func newApp(logger tlog.Logger, db dbm.DB, traceStore io.Writer, appOpts servert
 		db,
 		-1, /* height */
 		skipUpgradeHeights,
-		nil,
+		a.Context.Config,
 		baseapp.SetHaltHeight(cast.ToUint64(appOpts.Get(server.FlagHaltHeight))),
 		baseapp.SetHaltTime(cast.ToUint64(appOpts.Get(server.FlagHaltTime))),
 		baseapp.SetInterBlockCache(cache),
@@ -74,7 +116,7 @@ func newApp(logger tlog.Logger, db dbm.DB, traceStore io.Writer, appOpts servert
 	)
 }
 
-func exportAppStateAndTMValidators(
+func (a appCreator) exportAppStateAndTMValidators(
 	logger tlog.Logger, db dbm.DB, traceStore io.Writer, height int64, forZeroHeight bool, jailWhiteList []string,
 	appOpts servertypes.AppOptions) (servertypes.ExportedApp, error) {
 	sgnApp := app.NewSgnApp(logger, db, height, map[int64]bool{}, nil)
@@ -83,4 +125,39 @@ func exportAppStateAndTMValidators(
 
 func addModuleInitFlags(startCmd *cobra.Command) {
 	crisis.AddModuleInitFlags(startCmd)
+}
+
+func initAppConfig() (string, interface{}) {
+	type WASMConfig struct {
+		// This is the maximum sdk gas (wasm and storage) that we allow for any x/wasm "smart" queries
+		QueryGasLimit uint64 `mapstructure:"query_gas_limit"`
+		// Address defines the gRPC-web server to listen on
+		LruSize uint64 `mapstructure:"lru_size"`
+	}
+
+	type CustomAppConfig struct {
+		serverconfig.Config
+		WASM WASMConfig `mapstructure:"wasm"`
+	}
+
+	srvCfg := serverconfig.DefaultConfig()
+	srvCfg.MinGasPrices = "0stake"
+
+	customAppConfig := CustomAppConfig{
+		Config: *srvCfg,
+		WASM: WASMConfig{
+			LruSize:       1,
+			QueryGasLimit: 300000,
+		},
+	}
+
+	customAppTemplate := serverconfig.DefaultConfigTemplate + `
+[wasm]
+# This is the maximum sdk gas (wasm and storage) that we allow for any x/wasm "smart" queries
+query_gas_limit = 300000
+# This is the number of wasm vm instances we keep cached in memory for speed-up
+# Warning: this is currently unstable and may lead to crashes, best to keep for 0 unless testing locally
+lru_size = 0`
+
+	return customAppTemplate, customAppConfig
 }
