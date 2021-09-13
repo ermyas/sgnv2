bf68fcb6783c81fbf92bc6820a93812dc831eabd
Junda Liu <junda@celer.network>
2021-09-12 20:31:29 -0700

    add cbr_puller.go

---
+++ relayer/cbr_puller.go
@@ -0,0 +1,31 @@
+package relayer
+
+import (
+	"time"
+
+	storetypes "github.com/cosmos/cosmos-sdk/store/types"
+)
+
+// sleep, check if syncer, if yes, go over cbr dbs to send tx
+func (r *Relayer) doCbridge(cbrMgr CbrMgr) {
+	for {
+		time.Sleep(15 * time.Second)
+		if !r.isSyncer() {
+			continue
+		}
+		// find all events need to be sent out, batch into one propose msg
+		for chid, onech := range cbrMgr {
+			// go over each chain db events, send msg
+			onech.pullEvents(chid)
+		}
+	}
+}
+
+func (r *Relayer) sendCbrMsg() {}
+
+func (c *CbrOneChain) pullEvents(chid uint64) {
+	for _, evn := range evNames {
+		c.db.ReverseIterator([]byte(evn), storetypes.PrefixEndBytes([]byte(evn)))
+
+	}
+}
