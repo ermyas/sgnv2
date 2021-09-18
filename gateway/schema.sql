47a96029bb8f482ef0010cf6854e3f48d5bb0a2c
zhangqi021 <52643811+zhangqi021@users.noreply.github.com>
2021-09-18 19:23:09 +0800

    add schema (#115)

--- gateway/schema.sql
+++ gateway/schema.sql
@@ -10,16 +10,47 @@ CREATE TABLE IF NOT EXISTS transfer (
     token_symbol TEXT NOT NULL,
     src_chain_id INT NOT NULL,
     dst_chain_id INT NOT NULL,
-    src_token_info TEXT NOT NULL DEFAULT '',
-    dst_token_info TEXT NOT NULL DEFAULT '',
-    src_chain_info TEXT NOT NULL DEFAULT '',
-    dst_chain_info TEXT NOT NULL DEFAULT '',
-    amt float NOT NULL DEFAULT 0,
-    received_amt float NOT NULL DEFAULT 0,
+    amt string NOT NULL DEFAULT '0',
+    received_amt string NOT NULL DEFAULT 0,
     status INT NOT NULL DEFAULT 1,
     volume float NOT NULL DEFAULT 0
 );
 CREATE INDEX IF NOT EXISTS tsf_utm_idx ON transfer (update_time);
 CREATE INDEX IF NOT EXISTS tsf_ctm_idx ON transfer (create_time);
-CREATE INDEX IF NOT EXISTS tsf_sdr_idx ON transfer (usr_addr);
-CREATE INDEX IF NOT EXISTS tsf_rcr_idx ON transfer (transfer_id);
+CREATE INDEX IF NOT EXISTS tsf_addr_idx ON transfer (usr_addr);
+CREATE INDEX IF NOT EXISTS tsf_tid_idx ON transfer (transfer_id);
+
+CREATE TABLE IF NOT EXISTS lp (
+    usr_addr TEXT NOT NULL,
+    chain_id INT NOT NULL,
+    token_symbol TEXT NOT NULL,
+    amt string NOT NULL DEFAULT '0',
+    update_time TIMESTAMPTZ NOT NULL DEFAULT now(),
+    create_time TIMESTAMPTZ NOT NULL DEFAULT now(),
+    status INT NOT NULL DEFAULT 1,
+    type INT NOT NULL DEFAULT 1,
+    withdraw_id string NOT NULL DEFAULT '',
+    seq_num INT NOT NULL DEFAULT 0,
+    volume float NOT NULL DEFAULT 0
+    );
+CREATE INDEX IF NOT EXISTS lp_utm_idx ON lp (update_time);
+CREATE INDEX IF NOT EXISTS lp_ctm_idx ON lp (create_time);
+CREATE INDEX IF NOT EXISTS lp_addr_idx ON lp (usr_addr);
+
+
+CREATE TABLE IF NOT EXISTS token (
+    symbol TEXT NOT NULL,
+    chain_id INT NOT NULL,
+    decimal INT NOT NULL,
+    address TEXT NOT NULL,
+    name TEXT NOT NULL DEFAULT '',
+    icon TEXT NOT NULL DEFAULT '',
+    PRIMARY KEY (symbol,chain_id)
+);
+CREATE INDEX IF NOT EXISTS tk_addr_idx ON token (address, chain_id);
+
+CREATE TABLE IF NOT EXISTS chain (
+     id INT PRIMARY KEY NOT NULL,
+     name TEXT NOT NULL DEFAULT '',
+     icon TEXT NOT NULL DEFAULT '',
+);
