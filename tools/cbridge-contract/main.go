d1156ce1cfbcbc7b0258baa463b86b8766a98648
i9 <i9@users.noreply.github.com>
2021-09-29 16:40:40 -0700

    tool to call Initsigner and setwrap (#179)
    * call init signer
    
    * add setwrap

--- tools/cbridge-contract/main.go
+++ tools/cbridge-contract/main.go
@@ -9,7 +9,9 @@ import (
 	"strings"
 
 	"github.com/celer-network/sgn-v2/eth"
+	cbrtypes "github.com/celer-network/sgn-v2/x/cbridge/types"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
@@ -22,6 +24,12 @@ var (
 	weth = flag.String("weth", "", "weth hex address")
 )
 
+const (
+	// from genesis.json staking validators
+	valEth = "0xa500023551388763b720808c0b0cdf00a752b69f"
+	valAmt = "1000000000000"
+)
+
 func main() {
 	flag.Parse()
 	ec, err := ethclient.Dial(*gw)
@@ -30,6 +38,27 @@ func main() {
 	chkErr(err, "")
 	log.Print("chainid: ", chainid)
 	auth, err := kspath2auth(*ks, chainid)
+	if *cbr != "" {
+		cbridge, _ := eth.NewBridge(eth.Hex2Addr(*cbr), ec)
+		var tx *types.Transaction
+		if *weth != "" {
+			// set weth
+			wethAddr := eth.Hex2Addr(*weth)
+			tx, err = cbridge.SetWrap(auth, wethAddr)
+		} else {
+			// init signer
+			amt, _ := new(big.Int).SetString(valAmt, 10)
+			signers := new(cbrtypes.SortedSigners)
+			signers.Signers = append(signers.Signers, &cbrtypes.AddrAmt{
+				Addr: eth.Hex2Bytes(valEth),
+				Amt:  amt.Bytes(),
+			})
+			raw, _ := signers.Marshal()
+			tx, err = cbridge.SetInitSigners(auth, raw)
+		}
+		log.Print(tx.Hash().String(), err)
+		return
+	}
 	addr, _, _, _ := eth.DeployBridge(auth, ec, nil)
 	log.Print("cbridge addr: ", addr.String())
 }
