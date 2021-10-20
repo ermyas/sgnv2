4eaed847ffd10439a166a6c9969bca1d38edbaca
zhangqi021 <52643811+zhangqi021@users.noreply.github.com>
2021-10-20 17:10:21 +0800

    Staking (#301)

---
+++ gateway/svc/staking.go
@@ -0,0 +1,16 @@
+package gatewaysvc
+
+import (
+	"context"
+	"github.com/celer-network/sgn-v2/common"
+	"github.com/celer-network/sgn-v2/gateway/webapi"
+	"github.com/spf13/viper"
+)
+
+func (gs *GatewayService) StakingConfig(ctx context.Context, request *webapi.StakingConfigRequest) (*webapi.StakingConfigResponse, error) {
+	return &webapi.StakingConfigResponse{
+		ViewerContract:        viper.GetString(common.FlagEthContractViewer),
+		StakingContract:       viper.GetString(common.FlagEthContractStaking),
+		StakingRewardContract: viper.GetString(common.FlagEthContractStakingReward),
+	}, nil
+}
--- gateway/webapi/gateway.pb.gw.go
+++ gateway/webapi/gateway.pb.gw.go
@@ -609,6 +609,24 @@ func local_request_Web_UpdateToken_0(ctx context.Context, marshaler runtime.Mars
 
 }
 
+func request_Web_StakingConfig_0(ctx context.Context, marshaler runtime.Marshaler, client WebClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq StakingConfigRequest
+	var metadata runtime.ServerMetadata
+
+	msg, err := client.StakingConfig(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_Web_StakingConfig_0(ctx context.Context, marshaler runtime.Marshaler, server WebServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq StakingConfigRequest
+	var metadata runtime.ServerMetadata
+
+	msg, err := server.StakingConfig(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
 // RegisterWebHandlerServer registers the http handlers for service Web to "mux".
 // UnaryRPC     :call WebServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
@@ -1006,6 +1024,29 @@ func RegisterWebHandlerServer(ctx context.Context, mux *runtime.ServeMux, server
 
 	})
 
+	mux.Handle("GET", pattern_Web_StakingConfig_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_Web_StakingConfig_0(rctx, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_Web_StakingConfig_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -1387,6 +1428,26 @@ func RegisterWebHandlerClient(ctx context.Context, mux *runtime.ServeMux, client
 
 	})
 
+	mux.Handle("GET", pattern_Web_StakingConfig_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_Web_StakingConfig_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_Web_StakingConfig_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -1424,6 +1485,8 @@ var (
 	pattern_Web_UpdateChain_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "config", "updateChain"}, "", runtime.AssumeColonVerbOpt(true)))
 
 	pattern_Web_UpdateToken_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "config", "updateToken"}, "", runtime.AssumeColonVerbOpt(true)))
+
+	pattern_Web_StakingConfig_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "staking", "stakingConfig"}, "", runtime.AssumeColonVerbOpt(true)))
 )
 
 var (
@@ -1460,4 +1523,6 @@ var (
 	forward_Web_UpdateChain_0 = runtime.ForwardResponseMessage
 
 	forward_Web_UpdateToken_0 = runtime.ForwardResponseMessage
+
+	forward_Web_StakingConfig_0 = runtime.ForwardResponseMessage
 )
