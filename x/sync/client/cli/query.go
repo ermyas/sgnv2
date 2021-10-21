6f683a50a0cca730c7e70f41359fbc62d8bd29f9
Xiaozhou Li <xli@celer.network>
2021-10-21 12:25:48 -0700

    add sync params query

--- x/sync/client/cli/query.go
+++ x/sync/client/cli/query.go
@@ -1,6 +1,9 @@
 package cli
 
 import (
+	"fmt"
+
+	"github.com/celer-network/goutils/log"
 	"github.com/celer-network/sgn-v2/common"
 	"github.com/celer-network/sgn-v2/x/sync/types"
 	"github.com/cosmos/cosmos-sdk/client"
@@ -14,6 +17,37 @@ func GetQueryCmd() *cobra.Command {
 		SuggestionsMinimumDistance: 2,
 		RunE:                       client.ValidateCmd,
 	}
-	syncQueryCmd.AddCommand(common.GetCommands()...)
+	syncQueryCmd.AddCommand(common.GetCommands(GetCmdQueryParams())...)
 	return syncQueryCmd
+}
+
+func GetCmdQueryParams() *cobra.Command {
+	return &cobra.Command{
+		Use:   "params",
+		Args:  cobra.NoArgs,
+		Short: "Query the current staking parameters information",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cliCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+			params, err := QueryParams(cliCtx)
+			if err != nil {
+				log.Errorln("query error", err)
+				return err
+			}
+			return cliCtx.PrintProto(&params)
+		},
+	}
+}
+
+func QueryParams(cliCtx client.Context) (params types.Params, err error) {
+	route := fmt.Sprintf("custom/%s/%s", types.QuerierRoute, types.QueryParams)
+	res, err := common.RobustQuery(cliCtx, route)
+	if err != nil {
+		return
+	}
+
+	err = cliCtx.LegacyAmino.UnmarshalJSON(res, &params)
+	return
 }
