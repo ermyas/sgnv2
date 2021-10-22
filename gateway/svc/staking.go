146131fc13e95dd30482516b060b052975c83211
zhangqi021 <52643811+zhangqi021@users.noreply.github.com>
2021-10-22 10:41:16 +0800

    claim delegator reward (#312)

--- gateway/svc/staking.go
+++ gateway/svc/staking.go
@@ -2,8 +2,13 @@ package gatewaysvc
 
 import (
 	"context"
+	"github.com/celer-network/goutils/log"
 	"github.com/celer-network/sgn-v2/common"
+	"github.com/celer-network/sgn-v2/eth"
+	"github.com/celer-network/sgn-v2/gateway/dal"
 	"github.com/celer-network/sgn-v2/gateway/webapi"
+	"github.com/celer-network/sgn-v2/x/distribution/client/cli"
+	"github.com/celer-network/sgn-v2/x/distribution/types"
 	"github.com/spf13/viper"
 )
 
@@ -13,5 +18,57 @@ func (gs *GatewayService) StakingConfig(ctx context.Context, request *webapi.Sta
 		StakingContract:       viper.GetString(common.FlagEthContractStaking),
 		StakingRewardContract: viper.GetString(common.FlagEthContractStakingReward),
 		CelrContract:          viper.GetString(common.FlagEthContractCelr),
+	}, nil
+}
+
+func (gs *GatewayService) UnlockStakingReward(ctx context.Context, request *webapi.UnlockStakingRewardRequest) (*webapi.UnlockStakingRewardResponse, error) {
+	tr := gs.TP.GetTransactor()
+	_, err := cli.ClaimAllStakingReward(tr, &types.MsgClaimAllStakingReward{
+		DelegatorAddress: eth.Addr2Hex(common.Hex2Addr(request.GetDelegatorAddress())),
+		Sender:           tr.Key.GetAddress().String(),
+	})
+	if err != nil {
+		return &webapi.UnlockStakingRewardResponse{
+			Err: &webapi.ErrMsg{
+				Code: webapi.ErrCode_ERROR_CODE_COMMON,
+				Msg:  err.Error(),
+			},
+		}, nil
+	} else {
+		dberr := dal.DB.InsertClaimWithdrawRewardLog(request.GetDelegatorAddress())
+		if dberr != nil {
+			log.Errorf("InsertClaimWithdrawRewardLog failed, error:%+v", dberr)
+		}
+		return &webapi.UnlockStakingRewardResponse{}, nil
+	}
+}
+
+func (gs *GatewayService) GetStakingRewardDetails(ctx context.Context, request *webapi.GetStakingRewardDetailsRequest) (*webapi.GetStakingRewardDetailsResponse, error) {
+	tr := gs.TP.GetTransactor()
+	queryClient := types.NewQueryClient(tr.CliCtx)
+	res, err := queryClient.StakingRewardClaimInfo(
+		ctx,
+		&types.QueryStakingRewardClaimInfoRequest{
+			DelegatorAddress: common.Hex2Addr(request.GetDelegatorAddress()).String(),
+		},
+	)
+	if res == nil || err != nil {
+		log.Warnf("check failed, error:%+v", err)
+		return &webapi.GetStakingRewardDetailsResponse{
+			Err: &webapi.ErrMsg{
+				Code: webapi.ErrCode_ERROR_CODE_COMMON,
+				Msg:  "check failed",
+			},
+		}, nil
+	}
+	rewardClaimInfo := res.GetRewardClaimInfo()
+	return &webapi.GetStakingRewardDetailsResponse{
+		Detail: &types.StakingRewardClaimInfo{
+			Recipient:              rewardClaimInfo.GetRecipient(),
+			LastClaimTime:          rewardClaimInfo.GetLastClaimTime(),
+			CumulativeRewardAmount: rewardClaimInfo.GetCumulativeRewardAmount(),
+			RewardProtoBytes:       rewardClaimInfo.GetRewardProtoBytes(),
+			Signatures:             rewardClaimInfo.GetSignatures(),
+		},
 	}, nil
 }
--- gateway/webapi/gateway.pb.gw.go
+++ gateway/webapi/gateway.pb.gw.go
@@ -627,6 +627,76 @@ func local_request_Web_StakingConfig_0(ctx context.Context, marshaler runtime.Ma
 
 }
 
+func request_Web_UnlockStakingReward_0(ctx context.Context, marshaler runtime.Marshaler, client WebClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq UnlockStakingRewardRequest
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.UnlockStakingReward(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_Web_UnlockStakingReward_0(ctx context.Context, marshaler runtime.Marshaler, server WebServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq UnlockStakingRewardRequest
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := server.UnlockStakingReward(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
+var (
+	filter_Web_GetStakingRewardDetails_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
+)
+
+func request_Web_GetStakingRewardDetails_0(ctx context.Context, marshaler runtime.Marshaler, client WebClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq GetStakingRewardDetailsRequest
+	var metadata runtime.ServerMetadata
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Web_GetStakingRewardDetails_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.GetStakingRewardDetails(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_Web_GetStakingRewardDetails_0(ctx context.Context, marshaler runtime.Marshaler, server WebServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq GetStakingRewardDetailsRequest
+	var metadata runtime.ServerMetadata
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Web_GetStakingRewardDetails_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := server.GetStakingRewardDetails(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
 // RegisterWebHandlerServer registers the http handlers for service Web to "mux".
 // UnaryRPC     :call WebServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
@@ -1047,6 +1117,52 @@ func RegisterWebHandlerServer(ctx context.Context, mux *runtime.ServeMux, server
 
 	})
 
+	mux.Handle("POST", pattern_Web_UnlockStakingReward_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_Web_UnlockStakingReward_0(rctx, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_Web_UnlockStakingReward_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
+	mux.Handle("GET", pattern_Web_GetStakingRewardDetails_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_Web_GetStakingRewardDetails_0(rctx, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_Web_GetStakingRewardDetails_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -1448,6 +1564,46 @@ func RegisterWebHandlerClient(ctx context.Context, mux *runtime.ServeMux, client
 
 	})
 
+	mux.Handle("POST", pattern_Web_UnlockStakingReward_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_Web_UnlockStakingReward_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_Web_UnlockStakingReward_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
+	mux.Handle("GET", pattern_Web_GetStakingRewardDetails_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_Web_GetStakingRewardDetails_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_Web_GetStakingRewardDetails_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -1487,6 +1643,10 @@ var (
 	pattern_Web_UpdateToken_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "config", "updateToken"}, "", runtime.AssumeColonVerbOpt(true)))
 
 	pattern_Web_StakingConfig_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "staking", "stakingConfig"}, "", runtime.AssumeColonVerbOpt(true)))
+
+	pattern_Web_UnlockStakingReward_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "staking", "unlockStakingReward"}, "", runtime.AssumeColonVerbOpt(true)))
+
+	pattern_Web_GetStakingRewardDetails_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "staking", "getStakingRewardDetails"}, "", runtime.AssumeColonVerbOpt(true)))
 )
 
 var (
@@ -1525,4 +1685,8 @@ var (
 	forward_Web_UpdateToken_0 = runtime.ForwardResponseMessage
 
 	forward_Web_StakingConfig_0 = runtime.ForwardResponseMessage
+
+	forward_Web_UnlockStakingReward_0 = runtime.ForwardResponseMessage
+
+	forward_Web_GetStakingRewardDetails_0 = runtime.ForwardResponseMessage
 )
