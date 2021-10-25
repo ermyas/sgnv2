a1c908349cf11ccf53c853bfd896dd478e14c769
i9 <i9@users.noreply.github.com>
2021-10-24 21:49:26 -0700

    add sync liqadd and send (#332)
    * add sync liqadd and send
    
    * better sync all events

--- ops/sync.go
+++ ops/sync.go
@@ -17,7 +17,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/version"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
-	ec "github.com/ethereum/go-ethereum/common"
+	ethtypes "github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -28,20 +28,102 @@ const (
 	FlagTxHash  = "txhash"
 )
 
+var (
+	chainid uint64
+	txhash  string
+	// receipt of the txhash
+	txReceipt *ethtypes.Receipt
+	cbr       *CbrOneChain
+)
+
 // GetSyncCmd
 func GetSyncCmd() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:                        "sync",
-		Short:                      "Sync a change from onchain to sidechain",
-		SuggestionsMinimumDistance: 2,
-		RunE:                       client.ValidateCmd,
+		Use:   "sync",
+		Short: "Sync an event from onchain to sidechain",
+		RunE:  client.ValidateCmd,
+		PersistentPreRun: func(cmd *cobra.Command, args []string) {
+			var err error
+			cbr, err = newOneChain(chainid)
+			if err != nil {
+				log.Fatal("newOneChain err:", err)
+			}
+			txReceipt, err = cbr.TransactionReceipt(context.Background(), eth.Hex2Hash(txhash))
+			if err != nil {
+				log.Fatal("TransactionReceipt err:", err)
+			}
+		},
 	}
 
 	cmd.AddCommand(common.PostCommands(
 		GetSyncSigners(),
+		GetSyncEvent(),
 	)...)
 
+	cmd.PersistentFlags().Uint64Var(&chainid, FlagChainId, 0, "which chainid to query tx hash")
+	cmd.PersistentFlags().StringVar(&txhash, FlagTxHash, "", "tx hash, will parse last event")
 	return cmd
+}
+
+func GetSyncEvent() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "event",
+		Short: "Sync event from onchain, automatically figure out which event based on elog",
+		Long: strings.TrimSpace(
+			fmt.Sprintf(`
+Example:
+$ %s ops sync event --chainid=883 --txhash="xxxxx"
+`,
+				version.AppName,
+			),
+		),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			elog := *txReceipt.Logs[len(txReceipt.Logs)-1]
+			evname, ev := parseEvAndName(cbr.contract, elog)
+			log.Info(ev.PrettyLog(chainid))
+			cliCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+			sendCbrOnchainEvent(cliCtx, chainid, evname, elog)
+			return nil
+		},
+	}
+	return cmd
+}
+
+/*
+another way is to use abi and event.Sig and compare to elog.Topics
+cbrabi, _ := abi.JSON(strings.NewReader(eth.BridgeABI))
+for evname, v := range cbrabi.Events {
+	if eth.Hex2Hash(v.Sig) == elog.Topics[0] {
+		// evname found
+	}
+}
+*/
+func parseEvAndName(cbr *cbrContract, elog ethtypes.Log) (string, hasPrettyLog) {
+	var ev hasPrettyLog
+	ev, err := cbr.ParseLiquidityAdded(elog)
+	if err == nil {
+		return cbrtypes.CbrEventLiqAdd, ev
+	}
+	ev, err = cbr.ParseSend(elog)
+	if err == nil {
+		return cbrtypes.CbrEventSend, ev
+	}
+	ev, err = cbr.ParseRelay(elog)
+	if err == nil {
+		return cbrtypes.CbrEventRelay, ev
+	}
+	ev, err = cbr.ParseWithdrawDone(elog)
+	if err == nil {
+		return cbrtypes.CbrEventWithdraw, ev
+	}
+	return "", nil
+}
+
+type hasPrettyLog interface {
+	PrettyLog(uint64) string
 }
 
 func GetSyncSigners() *cobra.Command {
@@ -62,29 +144,15 @@ $ %s ops sync signers --chainid=883 --txhash="xxxxx"
 				return err
 			}
 
-			chainId := viper.GetUint64(FlagChainId)
-			txHash := viper.GetString(FlagTxHash)
-
-			c, err := newOneChain(chainId)
-			if err != nil {
-				return err
-			}
-
-			receipt, err := c.TransactionReceipt(context.Background(), ec.HexToHash(txHash))
-			if err != nil {
-				log.Errorf("TransactionReceipt err: %s", err)
-				return err
-			}
-
-			elog := *receipt.Logs[len(receipt.Logs)-1]
-			ev, err := c.contract.ParseSignersUpdated(elog)
+			elog := *txReceipt.Logs[len(txReceipt.Logs)-1]
+			ev, err := cbr.contract.ParseSignersUpdated(elog)
 			if err != nil {
 				log.Errorf("ParseSignersUpdated err: %s", err)
 				return err
 			}
 
 			// check in store
-			storedChainSigners, err := cbrcli.QueryChainSigners(cliCtx, chainId)
+			storedChainSigners, err := cbrcli.QueryChainSigners(cliCtx, chainid)
 			if err != nil && !strings.Contains(err.Error(), "record not found") {
 				log.Errorf("QueryChainSigners err: %s", err)
 				return err
@@ -96,7 +164,7 @@ $ %s ops sync signers --chainid=883 --txhash="xxxxx"
 			}
 
 			// check on chain
-			ssHash, err := c.contract.SsHash(&bind.CallOpts{})
+			ssHash, err := cbr.contract.SsHash(&bind.CallOpts{})
 			if err != nil {
 				log.Errorf("query ssHash err: %s", err)
 				return err
@@ -107,51 +175,47 @@ $ %s ops sync signers --chainid=883 --txhash="xxxxx"
 				return err
 			}
 
-			txr, err := transactor.NewTransactor(
-				cliCtx.HomeDir,
-				viper.GetString(common.FlagSgnChainId),
-				viper.GetString(common.FlagSgnNodeURI),
-				viper.GetString(common.FlagSgnValidatorAccount),
-				viper.GetString(common.FlagSgnPassphrase),
-				cliCtx.LegacyAmino,
-				cliCtx.Codec,
-				cliCtx.InterfaceRegistry,
-			)
-			if err != nil {
-				log.Errorf("NewTransactor err: %s", err)
-				return err
-			}
-
-			// find all events need to be sent out, batch into one msg
-			msg := &synctypes.MsgProposeUpdates{
-				Sender:  txr.Key.GetAddress().String(),
-				Updates: make([]*synctypes.ProposeUpdate, 0),
-			}
-
-			elogJson, _ := json.Marshal(elog)
-			onchev := &cbrtypes.OnChainEvent{
-				Chainid: chainId,
-				Evtype:  cbrtypes.CbrEventSignersUpdated,
-				Elog:    elogJson,
-			}
-			data, _ := onchev.Marshal()
-			msg.Updates = append(msg.Updates, &synctypes.ProposeUpdate{
-				Type:       synctypes.DataType_CbrOnchainEvent,
-				ChainId:    chainId,
-				ChainBlock: 0, // why do we need this in ProposeUpdate?
-				Data:       data,
-			})
-
-			txr.CliSendTxMsgWaitMined(msg)
-
+			sendCbrOnchainEvent(cliCtx, chainid, cbrtypes.CbrEventSignersUpdated, elog)
 			return nil
 		},
 	}
-
-	cmd.Flags().String(FlagChainId, "", "chain id")
-	cmd.Flags().String(FlagTxHash, "", "the hash of the tx which triggered the SignersUpdated event")
-	cmd.MarkFlagRequired(FlagChainId)
-	cmd.MarkFlagRequired(FlagTxHash)
-
 	return cmd
+}
+
+func sendCbrOnchainEvent(cliCtx client.Context, chainid uint64, evtype string, elog ethtypes.Log) error {
+	txr, err := transactor.NewTransactor(
+		cliCtx.HomeDir,
+		viper.GetString(common.FlagSgnChainId),
+		viper.GetString(common.FlagSgnNodeURI),
+		viper.GetString(common.FlagSgnValidatorAccount),
+		viper.GetString(common.FlagSgnPassphrase),
+		cliCtx.LegacyAmino,
+		cliCtx.Codec,
+		cliCtx.InterfaceRegistry,
+	)
+	if err != nil {
+		return fmt.Errorf("NewTransactor err: %w", err)
+	}
+
+	// find all events need to be sent out, batch into one msg
+	msg := &synctypes.MsgProposeUpdates{
+		Sender:  txr.Key.GetAddress().String(),
+		Updates: make([]*synctypes.ProposeUpdate, 0),
+	}
+
+	elogJson, _ := json.Marshal(elog)
+	onchev := &cbrtypes.OnChainEvent{
+		Chainid: chainid,
+		Evtype:  evtype,
+		Elog:    elogJson,
+	}
+	data, _ := onchev.Marshal()
+	msg.Updates = append(msg.Updates, &synctypes.ProposeUpdate{
+		Type:    synctypes.DataType_CbrOnchainEvent,
+		ChainId: chainid,
+		Data:    data,
+	})
+
+	txr.CliSendTxMsgWaitMined(msg)
+	return nil
 }
