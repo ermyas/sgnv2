309146ba57b1e8477ac5270e54a202eb8561e65b
Elliott He <elliott.he@outlook.com>
2021-12-07 20:19:49 +0800

    don't return err if one chain is off shelf (#663)

--- x/cbridge/keeper/querier.go
+++ x/cbridge/keeper/querier.go
@@ -140,7 +140,8 @@ func queryChainTokensConfig(ctx sdk.Context, req abci.RequestQuery, k Keeper, le
 	for _, a := range mca.Assets {
 		occ, ok := mccMap[a.ChainId]
 		if !ok {
-			return nil, fmt.Errorf("chain with Id %d is not configured", a.ChainId)
+			log.Warnln("chain %d is not configured in multichain, assume it's off shelf", a.ChainId)
+			occ = &common.OneChainConfig{}
 		}
 
 		chid := strconv.FormatUint(a.ChainId, 10)
