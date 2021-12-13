56eeb8b9362767034b84831f69b1f284ca57f6a6
Elliott He <elliott.he@outlook.com>
2021-12-13 14:53:34 +0800

    cli cmd to trigger mint/wd sign (#718)

--- x/pegbridge/client/cli/tx.go
+++ x/pegbridge/client/cli/tx.go
@@ -6,10 +6,12 @@ import (
 
 	"github.com/spf13/cobra"
 
-	"github.com/cosmos/cosmos-sdk/client"
-	// "github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/celer-network/goutils/log"
+	"github.com/celer-network/sgn-v2/common"
 	"github.com/celer-network/sgn-v2/transactor"
 	"github.com/celer-network/sgn-v2/x/pegbridge/types"
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
 )
 
 var (
@@ -30,7 +32,10 @@ func GetTxCmd() *cobra.Command {
 		RunE:                       client.ValidateCmd,
 	}
 
-	// this line is used by starport scaffolding # 1
+	cmd.AddCommand(common.PostCommands(
+		GetCmdSignAgainMint(),
+		GetCmdSignAgainWithdraw(),
+	)...)
 
 	return cmd
 }
@@ -39,4 +44,76 @@ func InitClaimFee(t *transactor.Transactor, req *types.MsgClaimFee) (resp *types
 	req.Sender = t.Key.GetAddress().String() // make sure the msg sender is the transactor
 	_, err = t.LockSendTx(req)
 	return
+}
+
+func GetCmdSignAgainMint() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "sign-again-mint",
+		Short: "Trigger sign again of mint msg",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+			home, err := cmd.Flags().GetString(flags.FlagHome)
+			if err != nil {
+				return err
+			}
+			txr, err := transactor.NewCliTransactor(home, clientCtx.LegacyAmino, clientCtx.Codec, clientCtx.InterfaceRegistry)
+			if err != nil {
+				log.Error(err)
+				return err
+			}
+			msg := &types.MsgTriggerSignMint{
+				MintId: args[0],
+				Sender: txr.Key.GetAddress().String(),
+			}
+			err = msg.ValidateBasic()
+			if err != nil {
+				return err
+			}
+
+			txr.CliSendTxMsgWaitMined(msg)
+
+			return nil
+		},
+	}
+	return cmd
+}
+
+func GetCmdSignAgainWithdraw() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "sign-again-withdraw",
+		Short: "Trigger sign again of withdraw msg",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+			home, err := cmd.Flags().GetString(flags.FlagHome)
+			if err != nil {
+				return err
+			}
+			txr, err := transactor.NewCliTransactor(home, clientCtx.LegacyAmino, clientCtx.Codec, clientCtx.InterfaceRegistry)
+			if err != nil {
+				log.Error(err)
+				return err
+			}
+			msg := &types.MsgTriggerSignWithdraw{
+				WithdrawId: args[0],
+				Sender:     txr.Key.GetAddress().String(),
+			}
+			err = msg.ValidateBasic()
+			if err != nil {
+				return err
+			}
+
+			txr.CliSendTxMsgWaitMined(msg)
+
+			return nil
+		},
+	}
+	return cmd
 }
