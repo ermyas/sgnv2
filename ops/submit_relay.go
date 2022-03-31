c5af7986f96ab519c7f256885725a24d2930798c
Xiaozhou Li <xli@celer.network>
2022-03-31 15:28:03 -0700

    update submit-relay

--- ops/submit_relay.go
+++ ops/submit_relay.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"strings"
 
+	ethutils "github.com/celer-network/goutils/eth"
 	"github.com/celer-network/goutils/log"
 	"github.com/celer-network/sgn-v2/common"
 	"github.com/celer-network/sgn-v2/eth"
@@ -21,7 +22,8 @@ import (
 )
 
 const (
-	FlagXFerId = "xferid"
+	FlagXferId       = "xferid"
+	FlagGasPriceGwei = "gasprice"
 )
 
 func SubmitRelayCmd() *cobra.Command {
@@ -42,7 +44,7 @@ $ %s ops submit-relay --xferid=xxxxx"
 				return err
 			}
 
-			xFerId := eth.Hex2Bytes(viper.GetString(FlagXFerId))
+			xferId := eth.Hex2Bytes(viper.GetString(FlagXferId))
 			txr, err := transactor.NewTransactor(
 				cliCtx.HomeDir,
 				viper.GetString(common.FlagSgnChainId),
@@ -57,63 +59,59 @@ $ %s ops submit-relay --xferid=xxxxx"
 				return fmt.Errorf("NewTransactor err: %w", err)
 			}
 
-			// logic below is very much like relayer/cbr_puller.go:88 submitRelay
-			logmsg := fmt.Sprintf("Process relay srcId %x", xFerId)
-
-			relay, err := cbrcli.QueryRelay(txr.CliCtx, xFerId)
+			relay, err := cbrcli.QueryRelay(txr.CliCtx, xferId)
 			if err != nil {
-				return fmt.Errorf("%s. QueryRelay err: %s", logmsg, err)
+				return fmt.Errorf("QueryRelay err: %w", err)
 			}
 
 			relayOnChain := new(cbrtypes.RelayOnChain)
 			err = relayOnChain.Unmarshal(relay.Relay)
 			if err != nil {
-				return fmt.Errorf("%s. Unmarshal relay.Relay err %s", logmsg, err)
+				return fmt.Errorf("Unmarshal relay.Relay err %w", err)
 			}
 
 			chainSigners, err := cbrcli.QueryChainSigners(cliCtx, relayOnChain.GetDstChainId())
 			if err != nil && !errors.Is(err, sdkerrors.ErrKeyNotFound) {
-				log.Errorf("QueryChainSigners err: %s", err)
-				return err
+				return fmt.Errorf("QueryChainSigners err: %w", err)
 			}
 			curss := chainSigners.GetSortedSigners()
 			pass, sigsBytes := cbrtypes.ValidateSigQuorum(relay.SortedSigs, curss)
 			if !pass {
-				log.Warnf("%s. Not have enough sigs %s, curss %x", logmsg, relay.SignersStr(), curss)
-				return nil
+				return fmt.Errorf("Not have enough sigs %s, curss %x", relay.SignersStr(), curss)
 			}
-			relayTransferId := relayOnChain.GetRelayOnChainTransferId()
-			logmsg = fmt.Sprintf("%s dstId %x", logmsg, relayTransferId)
+			relayId := relayOnChain.GetRelayOnChainTransferId()
 			cbr, err := newOneChain(relayOnChain.GetDstChainId())
 			if err != nil {
-				log.Fatal("newOneChain err:", err)
+				return fmt.Errorf("newOneChain err: %w", err)
 			}
-			existRelay, existRelayErr := cbr.cbrContract.BridgeCaller.Transfers(&bind.CallOpts{}, relayTransferId)
-			if existRelayErr != nil {
-				// if fail to query, continue to send this relay, because we can not make sure whether the relay already exist.
-				log.Warnln("fail to query transfer err:", existRelayErr)
+			existRelay, err := cbr.cbrContract.BridgeCaller.Transfers(&bind.CallOpts{}, relayId)
+			if err != nil {
+				return fmt.Errorf("fail to query transfer err: %w", err)
 			} else if existRelay {
-				log.Infof("%s. dest transfer already exist on chain, skip it", logmsg)
+				log.Infof("dest transfer %x already exist on chain, skip it", relayId)
 				return nil
 			}
-			logmsg = fmt.Sprintf("srcXferId %x chain %d->%d", relayOnChain.GetSrcTransferId(), relayOnChain.GetSrcChainId(), relayOnChain.GetDstChainId())
-			re, err := cbr.Transactor.TransactWaitMined(
-				"cli submit relay",
+			logmsg := fmt.Sprintf("chain %d->%d transfer %x->%x",
+				relayOnChain.GetSrcChainId(), relayOnChain.GetDstChainId(), xferId, relayId)
+			tx, err := cbr.Transactor.Transact(
+				nil,
 				func(transactor bind.ContractTransactor, opts *bind.TransactOpts) (*ethtypes.Transaction, error) {
 					signers, powers := cbrtypes.SignersToEthArrays(curss)
 					return cbr.cbrContract.Relay(opts, relay.Relay, sigsBytes, signers, powers)
 				},
+				ethutils.WithForceGasGwei(viper.GetUint64(FlagGasPriceGwei)),
 			)
 			if err != nil {
 				return err
 			}
-			log.Infof("%s. tx hash %s", logmsg, re.TxHash.Hex())
+			log.Infof("%s. tx hash %x, gas %s", logmsg, tx.Hash(), tx.GasPrice())
 			return nil
 		},
 	}
 
-	cmd.Flags().String(FlagXFerId, "", "transferId, used to retry submit relay")
-	cmd.MarkFlagRequired(FlagXFerId)
+	cmd.Flags().String(FlagXferId, "", "transferId, used to retry submit relay")
+	cmd.Flags().Uint64(FlagGasPriceGwei, 0, "force gas price in gwei")
+	cmd.MarkFlagRequired(FlagXferId)
 
 	return cmd
 }
